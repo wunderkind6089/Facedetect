0c355653792079f46a6f28a1b40322fe00a6a464
Abdulquadri <olayanju.olaide@olivetsolutions>
2019-05-31 19:26:10 +0100

    Updates

---
+++ precache-manifest.e92407154e8c629d14175ec0af9ab0f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7c442d7911bfee74206",
+    "url": "/Facedetect/static/js/runtime~main.90f75fb3.js"
+  },
+  {
+    "revision": "ae1d1c017c055c7a0ff7",
+    "url": "/Facedetect/static/js/main.7ec0bd05.chunk.js"
+  },
+  {
+    "revision": "0433121592ed69aea8eb",
+    "url": "/Facedetect/static/js/2.34b68bff.chunk.js"
+  },
+  {
+    "revision": "ae1d1c017c055c7a0ff7",
+    "url": "/Facedetect/static/css/main.295d1ca1.chunk.css"
+  },
+  {
+    "revision": "0433121592ed69aea8eb",
+    "url": "/Facedetect/static/css/2.04f94a5a.chunk.css"
+  },
+  {
+    "revision": "0da3cf96dd51b002f9597824ecf1219d",
+    "url": "/Facedetect/index.html"
+  }
+];
